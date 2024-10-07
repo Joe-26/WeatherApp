fca218cb7456f72e47c048301d7528beb62ecd5a
Josephraj Velpula <joseph@Joes-MacBook.local>
2024-10-06 23:35:13 -0400

    Created a function for temperature API call & introduced formatting logic in index.html

--- weatherApp/meteo/views.py
+++ weatherApp/meteo/views.py
@@ -8,34 +8,30 @@ from datetime import datetime
 import random
 
 # Create your views here.
-def temp_here(request):
-    location = geocoder.ip('me').latlng
+def get_temp(location):
     endpoint = "https://api.open-meteo.com/v1/forecast"
     api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m&temperature_unit=fahrenheit"
     response = requests.get(api_request).json()
-    
+
     now = datetime.now()
     hour = now.hour
 
     currTemp = response['hourly']['temperature_2m'][hour]
 
+    return currTemp
+
+def temp_here(request):
+    location = geocoder.ip('me').latlng
+
     template = loader.get_template('index.html')
-    context = {'city':'your Location','temp': currTemp}
+    context = {'city':'your Location','temp': get_temp(location)}
     return HttpResponse(template.render(context, request))
 
 def temp_other(request):
     cities = WorldCities.objects.all()
-
-    location = random.choice(cities)
-    endpoint = "https://api.open-meteo.com/v1/forecast"
-    api_request = f"{endpoint}?latitude={location.lat}&longitude={location.lng}&hourly=temperature_2m&temperature_unit=fahrenheit"
-    response = requests.get(api_request).json()
-
-    now = datetime.now()
-    hour = now.hour
-
-    currTemp = response['hourly']['temperature_2m'][hour]
+    city = random.choice(cities)
+    location = [city.lat, city.lng]
 
     template = loader.get_template('index.html')
-    context = {'city':location.city, 'temp': currTemp}
+    context = {'city':city.city, 'country':city.country, 'temp': get_temp(location)}
     return HttpResponse(template.render(context, request))
